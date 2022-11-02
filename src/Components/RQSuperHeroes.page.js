d8704e84e42f38e37a7cf896363f7e04993589ac
theHabesa1 <yaredabera215@gmail.com>
2022-11-02 16:22:23 +0300

    cashe and staling test

--- src/Components/RQSuperHeroes.page.js
+++ src/Components/RQSuperHeroes.page.js
@@ -5,8 +5,12 @@ const fetchData = () => {
     return axios.get('http://localhost:4000/superheroes')
 }
 export const RQSuperHeroes = () => {
-   const{ isLoading, data, isError , error } = useQuery("super-heros",fetchData)
+   const{ isLoading, data, isError , error , isFetching } = useQuery("super-heros",fetchData,
+   {
+    staleTime:0,
+   })
 
+    console.log({isLoading,isFetching})
     if(isLoading){
         return <h2>Loading Superheroes......</h2>
     }
